0e3b0c5b2a22e705502a9b9c264cfb06826016c9
siddharth <siddharthjha79348@gmail.com>
2025-04-12 19:32:31 +0530

    first commit

---
+++ tipcalc.js
@@ -0,0 +1,15 @@
+let bill = document.getElementById("bill");
+let tip = document.getElementById("tip");
+let calculate = document.getElementById("calculate");
+let total = document.getElementById("total");
+
+function calculateTotal() {
+  let billAmount = bill.value;
+  let tipamount = tip.value;
+  let totalvalue = '$' + parseFloat(billAmount)+parseFloat(tipamount);
+
+  total.innerText = totalvalue;
+
+}
+console.log(calculate);
+calculate.addEventListener("click", calculateTotal);
